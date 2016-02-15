a1e0bd53c58aa1f0a01f13b536b5fd8a8985f813
April Rueb <aprilrueb@gmail.com>
2016-02-15 17:53:58 -0500

    Update cracklepop.R

--- cracklepop.R
+++ cracklepop.R
@@ -1,6 +1,11 @@
-#Write a program that prints out the numbers 1 to 100 (inclusive).
-#If the number is divisible by 3, print Crackle instead of the number.
-#If it's divisible by 5, print Pop. If it's divisible by both 3 and 5, print CracklePop.
+#Write a program that prints out the
+#numbers 1 to 100 (inclusive).
+#If the number is divisible by 3, print
+#Crackle instead of the number.
+#If it's divisible by 5, print Pop. If
+#it's divisible by both 3 and 5, print
+#CracklePop.
+
 numbers <- 1:100
 for(i in 1:length(numbers)){
   if(numbers[i] %% 3 == 0 & numbers[i] %% 5 == 0){
